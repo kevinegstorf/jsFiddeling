5cc8beb6c3498312c7584f8fa1187a6e5838747f
kevinegstorf <kevin.egstorf@gmail.com>
2016-07-12 20:44:43 +0200

    event listener example

---
+++ js/event-listener.js
@@ -0,0 +1,12 @@
+function checkUsername() {                                  //declare function
+  var elMsg = document.getElementById('feedback');          //get feedback element
+  if (this.value.length < 5) {                              //if username is too short
+    elMsg.textContent = 'Username must be 5 chars or more'; //Set msg
+  } else {                                                  //Oterwise
+    elMsg.textContent = '';                                 //msg is empty string
+  }
+}
+
+var elUsername = document.getElementById('username');      //get user input
+//when it loses focus call checkUsername()
+elUsername.addEventListener('blur', checkUsername, false);
